14a7c7e084404cec9ac1b42b396221c10fe0bf7b
Felipe Moura <f.santosdemoura0703@gmail.com>
2024-04-18 21:18:31 -0300

    Alterando o butom Result

--- calculadora_de_notas_unama/lib/components/butomResult.dart
+++ calculadora_de_notas_unama/lib/components/butomResult.dart
@@ -3,14 +3,14 @@ import 'package:flutter/widgets.dart';
 
 Widget buttonAgain(text) {
   return
-      // =====================================
+      // =======================================
       //      Buttom
-      // =====================================
+      // =======================================
       InkWell(
     onTap: () {
       print('O Resultado é igual -> ');
     },
-    //============================================
+    //==============================================
     //          F U N D O
     //============================================
     child: Container(
@@ -19,18 +19,18 @@ Widget buttonAgain(text) {
       alignment: Alignment.bottomCenter,
       decoration: BoxDecoration(
           boxShadow: [
-            //=======================================
+            //=========================================
             //          S O M B R A
-            //=======================================
+            //=========================================
             BoxShadow(
               color: Colors.grey.withOpacity(0.2), // Cor da sombra
               spreadRadius: 1, // Espalha a sombra para todas as direções
               blurRadius: 1, // Suaviza a borda da sombra
               offset: Offset(3, 3), // Deslocamento da sombra
             ),
-            //=======================================
+            //=========================================
             //        Cor e Texto do conteiner
-            //=======================================
+            //=========================================
           ],
           color: const Color(0xFF639976),
           borderRadius: BorderRadius.circular(10)),
@@ -45,7 +45,7 @@ Widget buttonAgain(text) {
     ),
   );
 
-  // =====================================
+  // =======================================
 }
 
 Widget buttonShare(text) {
@@ -53,34 +53,34 @@ Widget buttonShare(text) {
       onTap: () {
         // print('OK, CLIQUE DNV');
       },
-      //============================================
+      //==============================================
       //          F U N D O
-      //============================================
+      //==============================================
       child: Container(
           height: 50,
           width: 230,
           alignment: Alignment.bottomCenter,
           decoration: BoxDecoration(
               boxShadow: [
-                //=======================================
+                //=========================================
                 //          S O M B R A
-                //=======================================
+                //=========================================
                 BoxShadow(
                   color: Colors.grey.withOpacity(0.2), // Cor da sombra
                   spreadRadius: 1, // Espalha a sombra para todas as direções
                   blurRadius: 1, // Suaviza a borda da sombra
                   offset: Offset(3, 3), // Deslocamento da sombra
                 ),
-                //=======================================
+                //=========================================
                 //        Cor e Texto do conteiner
-                //=======================================
+                //=========================================
               ],
               color: const Color(0xFF639976),
               borderRadius: BorderRadius.circular(10)),
           child: Container(
             margin: EdgeInsets.only(left: 10, bottom: 15, top: 10, right: 10),
             child: const Text(
-              'Compartilhar resultado ',
+              'Compartilhar resultado',
               style: TextStyle(
                   fontSize: 16,
                   color: Colors.white,
